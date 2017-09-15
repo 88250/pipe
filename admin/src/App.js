4dc5491e8f8a723899fd9d6665f20fd866a5ba90
Vanessa <liliyuan@gmail.com>
2017-09-15 18:10:55 +0800

    test

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,30 +1,30 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Link, Switch } from 'react-router-dom'
 
-import SubRoutes from './components/SubRoutes'
 import routes from './routes'
+import SubRoutes from './components/SubRoutes'
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
-          <ul>
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/admin">Admin</Link></li>
-            <li><Link to="/admin/comments">comments</Link></li>
-            <li><Link to="/admin/settings">settings</Link></li>
-            <li><Link to="/admin/settings/users">users</Link></li>
-          </ul>
-
+        <ul>
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="/admin">Admin</Link></li>
+          <li><Link to="/admin/comments">comments</Link></li>
+          <li><Link to="/admin/settings">settings</Link></li>
+          <li><Link to="/admin/settings/users">users</Link></li>
+        </ul>
+        <Switch>
           {routes.map((route, i) => (
             <SubRoutes key={i} {...route}/>
           ))}
+        </Switch>
         </div>
       </Router>
     );
   }
 }
-
 
 export default App;
--- admin/src/components/SubRoutes.js
+++ admin/src/components/SubRoutes.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 
 const SubRoutes = (route) => (
-  <Route path={route.path} render={props => (
+  <Route path={route.path} exact render={props => (
     // pass the sub-routes down to keep nesting
     <route.component {...props} routes={route.routes}/>
   )}/>
