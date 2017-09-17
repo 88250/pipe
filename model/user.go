43422f971d49c40c80e49fc8b07ce6efe8dbf534
Liang Ding <DL88250@gmail.com>
2017-09-17 17:34:23 +0800

    :memo: Code comments

--- model/user.go
+++ model/user.go
@@ -2,6 +2,7 @@ package model
 
 import "github.com/jinzhu/gorm"
 
+// User model.
 type User struct {
 	gorm.Model
 	Name string `sql:"type:VARCHAR(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci"`
