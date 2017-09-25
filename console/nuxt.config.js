6ee59a7ea1172a18d56e059902aac1e7a774add1
Van <lly219@gmail.com>
2017-09-25 09:23:09 +0800

    test github account

--- console/nuxt.config.js
+++ console/nuxt.config.js
@@ -5,7 +5,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'Solo',
+    title: 'Solo.go',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
