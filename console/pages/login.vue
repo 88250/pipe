6cf1ae7863329e82cbdce36cdaccb9bcb9599bc1
Vanessa <lly219@gmail.com>
2017-09-30 11:27:02 +0800

    login add canvas

--- console/pages/login.vue
+++ console/pages/login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="login">
+  <div class="login" id="login">
     <div class="card">
       <div class="card__body fn-clear">
         <h2>{{ $t('login', $store.state.locale) }}</h2>
@@ -35,6 +35,8 @@
 
 <script>
   import md5 from 'blueimp-md5'
+  import 'particles.js'
+
   export default {
     layout: 'console',
     head () {
@@ -73,6 +75,96 @@
           this.$set(this, 'errorMsg', responseData.msg)
         }
       }
+    },
+    mounted () {
+      window.particlesJS('login', {
+        'particles': {
+          'number': {
+            'value': 6,
+            'density': {
+              'enable': true,
+              'value_area': 200
+            }
+          },
+          'color': {
+            'value': '#bbb'
+          },
+          'opacity': {
+            'value': 0.5,
+            'anim': {
+              'speed': 1,
+              'opacity_min': 0.1
+            }
+          },
+          'size': {
+            'value': 10,
+            'random': true,
+            'anim': {
+              'enable': false,
+              'speed': 80,
+              'size_min': 0.1,
+              'sync': false
+            }
+          },
+          'line_linked': {
+            'enable': true,
+            'distance': 300,
+            'color': '#bbb',
+            'opacity': 0.4,
+            'width': 1
+          },
+          'move': {
+            'enable': true,
+            'speed': 2,
+            'direction': 'none',
+            'straight': false,
+            'out_mode': 'out',
+            'bounce': false,
+            'attract': {
+              'enable': false,
+              'rotateX': 300,
+              'rotateY': 600
+            }
+          }
+        },
+        'interactivity': {
+          'detect_on': 'canvas',
+          'events': {
+            'onclick': {
+              'enable': false
+            },
+            'resize': true
+          },
+          'modes': {
+            'grab': {
+              'distance': 400,
+              'line_linked': {
+                'opacity': 0.7
+              }
+            },
+            'bubble': {
+              'distance': 800,
+              'size': 80,
+              'duration': 2,
+              'opacity': 0.8,
+              'speed': 3
+            },
+            'repulse': {
+              'distance': 400,
+              'duration': 0.4
+            },
+            'push': {
+              'particles_nb': 4
+            },
+            'remove': {
+              'particles_nb': 2
+            }
+          }
+        },
+        'retina_detect': true
+      }, function () {
+        console.log('callback - particles.js config loaded')
+      })
     }
   }
 </script>
@@ -82,7 +174,13 @@
   .login
     background-color: $blue-lighter
     padding: 50px 0
+    position: relative
+    .particles-js-canvas-el
+      position: absolute
+      top: 0
     .card
       width: 650px
       margin: 0 auto
+      position: relative
+      z-index: 1
 </style>
