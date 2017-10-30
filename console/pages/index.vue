1d73f076c6466702df112422deef42ec8769aea2
Vanessa <lly219@gmail.com>
2017-10-30 11:30:24 +0800

    index add blogs

--- console/pages/index.vue
+++ console/pages/index.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="welcome" id="index">
-    <div class="card card__body">
-      <h2>{{ $t('welcome', $store.state.locale) }}</h2>
+    <div class="card">
+      <h2 class="card__title">{{ $t('welcome', $store.state.locale) }}</h2>
+      <ul class="list" v-if="$store.state.role !== 0 && $store.state.role !== 4">
+        <li class="fn-flex" v-for="item in $store.state.blogs">
+          <a class="fn-flex-1" :href="item.url">{{ item.title }}</a>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -124,6 +129,7 @@
       margin: 0 auto
       position: relative
       z-index: 1
+
   @media (max-width: 768px)
     .welcome
       padding: 0 15px
