72f8ed8878aa44d4dae34001ec29e2afb1e2ea29
Vanessa <lly219@gmail.com>
2017-11-08 12:09:07 +0800

    blog manager

--- console/components/biz/Blog.vue
+++ console/components/biz/Blog.vue
@@ -2,59 +2,18 @@
   <div class="card__body fn-clear">
     <v-form ref="form">
       <v-text-field
-        :label="$t('blogTitle', $store.state.locale)"
-        v-model="blogTitle"
-      ></v-text-field>
-      <v-text-field
-        :label="$t('blogSubtitle', $store.state.locale)"
-        v-model="blogSubtitle"
-      ></v-text-field>
-      <v-text-field
-        :label="$t('faviconURL', $store.state.locale)"
-        v-model="faviconURL"
-      ></v-text-field>
-      <v-text-field
-        label="Meta Keywords"
-        v-model="metaKeywords"
-      ></v-text-field>
-      <v-text-field
-        label="Meta Description"
-        v-model="metaDescription"
-      ></v-text-field>
-      <v-text-field
-        label="HTML head"
-        v-model="header"
-      ></v-text-field>
-      <v-text-field
-        :label="$t('footer', $store.state.locale)"
-        v-model="footer"
-      ></v-text-field>
-      <v-text-field
-        :label="$t('noticeBoard', $store.state.locale)"
-        v-model="noticeBoard"
-      ></v-text-field>
-      <v-text-field
-        :label="$t('blogURL', $store.state.locale)"
-        v-model="blogURL"
-      ></v-text-field>
-      <v-text-field
-        :label="$t('commonUser', $store.state.locale)"
-        v-model="blogMembers"
-      ></v-text-field>
-      <v-text-field
         :label="$t('blogAdmin', $store.state.locale)"
-        v-model="blogAdmin"
+        v-model="name"
+        :counter="32"
+        :rules="requiredRules"
       ></v-text-field>
-      <label class="checkbox">
-        <input type="checkbox" :checked="commentable" @click="commentable = !commentable"/><span
-        class="checkbox__icon"></span>
-        {{ $t('allowComment', $store.state.locale) }}
-      </label>
+
       <div class="alert alert--danger" v-show="error">
         <v-icon>danger</v-icon>
         <span>{{ errorMsg }}</span>
       </div>
     </v-form>
+
     <v-btn class="fn-right btn--margin-t30 btn--info btn--space" @click="created">
       {{ $t('confirm', $store.state.locale) }}
     </v-btn>
@@ -65,34 +24,18 @@
 </template>
 
 <script>
+  import {required, maxSize} from '~/plugins/validate'
+
   export default {
-    props: {
-      id: {
-        type: Number,
-        required: true
-      }
-    },
     data () {
       return {
         errorMsg: '',
         error: false,
-        blogTitle: '',
-        blogSubtitle: '',
-        faviconURL: '',
-        header: '',
-        footer: '',
-        metaKeywords: '',
-        metaDescription: '',
-        noticeBoard: '',
-        blogURL: '',
-        blogMembers: '',
-        blogAdmin: '',
-        commentable: true
-      }
-    },
-    watch: {
-      id: function () {
-        this.init()
+        name: '',
+        requiredRules: [
+          (v) => required.call(this, v),
+          (v) => maxSize.call(this, v, 32)
+        ]
       }
     },
     methods: {
@@ -100,26 +43,9 @@
         if (!this.$refs.form.validate()) {
           return
         }
-        let responseData = {}
-        const requestData = {
-          blogTitle: this.blogTitle,
-          blogSubtitle: this.blogSubtitle,
-          faviconURL: this.faviconURL,
-          header: this.header,
-          footer: this.footer,
-          metaKeywords: this.metaKeywords,
-          metaDescription: this.metaDescription,
-          noticeBoard: this.noticeBoard,
-          blogURL: this.blogURL,
-          blogMembers: this.blogMembers,
-          blogAdmin: this.blogAdmin,
-          commentable: this.commentable
-        }
-        if (this.id === 0) {
-          responseData = await this.axios.post('/console/blogs', requestData)
-        } else {
-          responseData = await this.axios.put(`/console/blogs/${this.id}`, requestData)
-        }
+        const responseData = await this.axios.post('/console/blogs', {
+          name: this.name
+        })
 
         if (responseData.code === 0) {
           this.$set(this, 'error', false)
@@ -129,30 +55,7 @@
           this.$set(this, 'error', true)
           this.$set(this, 'errorMsg', responseData.msg)
         }
-      },
-      async init () {
-        if (this.id === 0) {
-          return
-        }
-        const responseData = await this.axios.get(`/console/blogs/${this.id}`)
-        if (responseData) {
-          this.$set(this, 'blogTitle', responseData.blogTitle)
-          this.$set(this, 'blogSubtitle', responseData.blogSubtitle)
-          this.$set(this, 'faviconURL', responseData.faviconURL)
-          this.$set(this, 'header', responseData.header)
-          this.$set(this, 'footer', responseData.footer)
-          this.$set(this, 'metaKeywords', responseData.metaKeywords)
-          this.$set(this, 'metaDescription', responseData.metaDescription)
-          this.$set(this, 'noticeBoard', responseData.noticeBoard)
-          this.$set(this, 'blogURL', responseData.blogURL)
-          this.$set(this, 'blogMembers', responseData.blogMembers)
-          this.$set(this, 'blogAdmin', responseData.blogAdmin)
-          this.$set(this, 'commentable', responseData.commentable)
-        }
       }
-    },
-    mounted () {
-      this.init()
     }
   }
 </script>
--- console/pages/admin/blogs/index.vue
+++ console/pages/admin/blogs/index.vue
@@ -1,49 +1,46 @@
 <template>
-  <div>
-    <div class="card fn-clear">
-      <blog v-if="showForm" :show.sync="showForm" @addSuccess="addSuccess" :id="editId"></blog>
+  <div class="card">
+    <blog v-if="showForm" :show.sync="showForm" @addSuccess="addSuccess"></blog>
 
-      <div v-show="!showForm" class="card__body fn-clear">
-        <v-btn class="btn--success fn-right" @click="edit(0)">{{ $t('new', $store.state.locale) }}</v-btn>
-      </div>
-      <ul class="list">
-        <li v-for="item in list" :key="item.id" class="fn-flex">
-          <div class="avatar avatar--mid avatar--space" :style="`background-image: url(${item.blogAdmin.avatarURL})`"></div>
-          <div class="fn-flex-1">
-            <div class="fn-flex">
-              <nuxt-link class="list__title fn-flex-1" target="_blank" :to="`${item.blogURL}`">
-                {{ item.blogTitle }}
-              </nuxt-link>
-              <v-menu
-                v-if="$store.state.role < 3"
-                :nudge-bottom="28"
-                :nudge-width="60"
-                :nudge-left="60"
-                :open-on-hover="true">
-                <v-toolbar-title slot="activator">
-                  <v-btn class="btn--small btn--info" @click="edit(item.id)">
-                    {{ $t('edit', $store.state.locale) }}
-                    <v-icon>arrow_drop_down</v-icon>
-                  </v-btn>
-                </v-toolbar-title>
-                <v-list>
-                  <v-list-tile>
-                    <v-list-tile-title>
-                      <div @click="edit(item.id)">{{ $t('edit', $store.state.locale) }}</div>
-                    </v-list-tile-title>
-                    <v-list-tile-title>
-                      <div @click="remove(item.id)">{{ $t('delete', $store.state.locale) }}</div>
-                    </v-list-tile-title>
-                  </v-list-tile>
-                </v-list>
-              </v-menu>
-            </div>
-            <div class="list__meta">
-              {{ item.blogSubtitle }}
-            </div>
+    <div v-show="!showForm" class="card__body fn-flex">
+      <v-text-field
+        v-if="list.length > 0"
+        @keyup.enter="getList()"
+        class="fn-flex-1"
+        :label="$t('enterSearch', $store.state.locale)"
+        v-model="keyword">
+      </v-text-field>
+      <v-btn class="btn--success" :class="{'btn--new': list.length > 0}" @click="edit">
+        {{ $t('new', $store.state.locale) }}
+      </v-btn>
+    </div>
+
+    <ul class="list" v-if="list.length > 0">
+      <li v-for="item in list" :key="item.id" class="fn-flex">
+        <div class="avatar avatar--mid avatar--space"
+             :style="`background-image: url(${item.blogLogo})`"></div>
+        <div class="fn-flex-1">
+          <div class="fn-flex">
+            <a class="list__title fn-flex-1" target="_blank" :href="`${item.blogURL}`">
+              {{ item.blogTitle }}
+            </a>
+            <v-btn class="btn--small btn--info" @click="prohibit(item.id, 'unprohibit')" v-if="item.status === 1">
+              {{ $t('unProhibit', $store.state.locale) }}
+            </v-btn>
+            <v-btn class="btn--small btn--danger" @click="prohibit(item.id, 'prohibit')" v-else>
+              {{ $t('prohibit', $store.state.locale) }}
+            </v-btn>
           </div>
-        </li>
-      </ul>
+          <div class="list__meta">
+            {{ item.blogAdmin }}
+            <span v-if="item.status === 1">
+               • <span class="ft-danger"> {{ $t('prohibit', $store.state.locale) }} </span>
+            </span>
+          </div>
+        </div>
+      </li>
+    </ul>
+    <div class="pagination--wrapper fn-clear" v-if="list.length > 0">
       <v-pagination
         :length="pageCount"
         v-model="currentPageNum"
@@ -67,7 +64,7 @@
     },
     data () {
       return {
-        editId: '',
+        keyword: '',
         showForm: false,
         currentPageNum: 1,
         pageCount: 1,
@@ -82,33 +79,31 @@
     },
     methods: {
       async getList (currentPage = 1) {
-        const responseData = await this.axios.get(`/console/blogs?p=${currentPage}`)
+        const responseData = await this.axios.get(`/console/blogs?p=${currentPage}&k=${this.keyword}`)
         if (responseData) {
-          this.$set(this, 'list', responseData.navigation)
+          this.$set(this, 'list', responseData.blogs)
           this.$set(this, 'currentPageNum', responseData.pagination.currentPageNum)
           this.$set(this, 'pageCount', responseData.pagination.pageCount)
           this.$set(this, 'windowSize', document.documentElement.clientWidth < 721 ? 5 : responseData.pagination.windowSize)
-        }
-      },
-      async remove (id) {
-        const responseData = await this.axios.delete(`/console/blogs/${id}`)
-        if (responseData === null) {
-          this.$store.commit('setSnackBar', {
-            snackBar: true,
-            snackMsg: this.$t('deleteSuccess', this.$store.state.locale),
-            snackModify: 'success'
-          })
-          this.getList()
-          this.$set(this, 'showForm', false)
         }
       },
       addSuccess () {
         this.getList()
         this.$set(this, 'showForm', false)
       },
-      edit (id) {
+      edit () {
         this.$set(this, 'showForm', true)
-        this.$set(this, 'editId', id)
+      },
+      async prohibit (id, type) {
+        const responseData = await this.axios.put(`/console/blogs/${id}/${type}`)
+        if (responseData.code === 0) {
+          this.$set(this, 'error', false)
+          this.$set(this, 'errorMsg', '')
+          this.getList()
+        } else {
+          this.$set(this, 'error', true)
+          this.$set(this, 'errorMsg', responseData.msg)
+        }
       }
     },
     mounted () {
