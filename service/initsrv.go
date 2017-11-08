5e3f109a92f7ec585497f380d361d31659a87f3a
Liang Ding <dl88250@gmail.com>
2017-11-08 17:05:15 +0800

    :speech_balloon: Init text literals

--- service/initsrv.go
+++ service/initsrv.go
@@ -214,11 +214,7 @@ func helloWorld(tx *gorm.DB, admin *model.User, blogID uint) error {
 
 欢迎使用 [Pipe](https://github.com/b3log/pipe) 博客平台。这是一篇自动生成的演示文章，编辑或者删除它，然后开始你的独立博客之旅！
 
-另外，欢迎你加入[黑客与画家的社区](https://hacpai.com)，你可以使用博客账号直接登录！
-
-----
-
-Pipe 博客系统是一个开源项目，如果你觉得它很赞，请到[项目首页](https://github.com/b3log/pipe)给颗星鼓励一下 :heart:`
+Pipe 博客平台是一个开源项目，如果你觉得它很赞，请到[项目首页](https://github.com/b3log/pipe)给颗星鼓励一下 :heart:`
 
 	article := &model.Article{
 		AuthorID:     admin.ID,
