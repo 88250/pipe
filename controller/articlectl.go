47b1d07263e943a14a50b604b5d695cf12b842b6
Liang Ding <dl88250@gmail.com>
2017-11-15 18:08:05 +0800

    :art: ToC

--- controller/articlectl.go
+++ controller/articlectl.go
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-// Package controller is the "controller" layer.
 package controller
 
 import (
