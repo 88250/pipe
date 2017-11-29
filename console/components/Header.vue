3a334bfc9016d4d9c9c6c3ad7139ef5e0cef326b
Liang Ding <DL88250@gmail.com>
2017-11-29 20:43:31 +0800

    :art: 管理状态

--- console/components/Header.vue
+++ console/components/Header.vue
@@ -57,7 +57,7 @@
             </v-list-tile>
             <v-list-tile
               @click="$router.push('/admin');$store.commit('setBodySide', 'body--side')"
-              v-if="$route.path.indexOf('/admin') === -1 && $store.state.role !== 0">
+              v-if="$route.path.indexOf('/admin') === -1 && $store.state.role !== 0 && $store.state.isInit">
                 {{ $t('manage', $store.state.locale) }}
             </v-list-tile>
             <v-list-tile @click="logout">
