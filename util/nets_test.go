67bcd8cc0ae2b860e31c47bb29923dda4ef82991
Liang Ding <dl88250@gmail.com>
2017-11-30 16:40:01 +0800

    :sparkles: Network utilities

--- util/nets_test.go
+++ util/nets_test.go
@@ -27,5 +27,37 @@ func TestIsIP(t *testing.T) {
 
 	if IsIP("is.not.an.ip") {
 		t.Errorf("[is.not.an.ip] is not an IP")
+
+		return
+	}
+
+	if !IsIP("127.0.0.1") {
+		t.Errorf("[127.0.0.1] is an IP")
+
+		return
+	}
+
+	if IsIP("localhost") {
+		t.Errorf("[localhost] is not an IP")
+
+		return
+	}
+}
+
+func TestIsDomain(t *testing.T) {
+	if !IsDomain("b3log.org") {
+		t.Errorf("[b3log.org] is a daomin")
+
+		return
+	}
+
+	if IsDomain("localhost") {
+		t.Errorf("[localhost] is not a domain")
+
+		return
+	}
+
+	if IsDomain("8.8.8.8") {
+		t.Errorf("[8.8.8.8] is not a domain")
 	}
 }
