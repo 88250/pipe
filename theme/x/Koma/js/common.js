d4ca02178e237290a62d875189de5e4727cc8767
Vanessa <lly219@gmail.com>
2017-12-13 13:18:58 +0800

    :art: toc

--- theme/x/Koma/js/common.js
+++ theme/x/Koma/js/common.js
@@ -30,9 +30,28 @@ const Common = {
       }, 800)
     });
 
+    $('#hideToc').click(function () {
+      $('.side > .fn-none').show()
+      $('.toc__panel').addClass('toc__panel--hide')
+      setTimeout(function () {
+        $('.toc__panel').hide();
+      }, 300)
+    });
+
+    $('#showToc').click(function () {
+      $('.toc__panel').show().removeClass('toc__panel--hide')
+      setTimeout(function () {
+        $('.side > .fn-none').hide()
+      }, 300)
+    });
+
     $(window).scroll(function () {
       if ($('.article__item').length > 0) {
-        $('.article__item:not(.article__item--show)').each(function () {
+        $('.article__item:not(.article__item--show)').each(function (index) {
+          if (index === 0) {
+            $(this).addClass('article__item--show');
+            return
+          }
           if ($(this).offset().top <= $(window).scrollTop() + $(window).height() - $(this).height() / 7) {
             $(this).addClass('article__item--show');
           }
