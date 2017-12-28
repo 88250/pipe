77470f98321702cb7d1160dd510464d8f51bb3df
Liang Ding <dl88250@gmail.com>
2017-12-28 17:55:10 +0800

    :bug: 修复搜索页面加载

--- controller/router.go
+++ controller/router.go
@@ -145,7 +145,7 @@ func MapRoutes() *gin.Engine {
 	if nil != err {
 		logger.Fatal("load theme templates failed: " + err.Error())
 	}
-	themeTemplates = append(themeTemplates, "theme/search/index.html")
+	themeTemplates = append(themeTemplates, staticPath("theme/search/index.html"))
 	commentTemplates, err := filepath.Glob(staticPath("theme/comment/*.html"))
 	if nil != err {
 		logger.Fatal("load comment templates failed: " + err.Error())
