06544eb7cc32bdc56f806f2ec38d457d7990ea1c
Van <lly219@gmail.com>
2018-07-02 11:38:20 +0800

    :art: #130

---
+++ console/pages/admin/settings/account/index.vue
@@ -0,0 +1,133 @@
+<template>
+  <div>
+    <div class="card fn-clear card__body">
+      <v-form ref="passwordForm">
+        <v-text-field
+          :label="$t('password', $store.state.locale)"
+          v-model="password"
+          type="password"
+          :rules="requiredRules"
+          @keyup.ctrl.13="passwordUpdate"
+          @keyup.meta.13="passwordUpdate"
+        ></v-text-field>
+
+        <div class="alert alert--danger" v-show="errorPassword">
+          <v-icon>danger</v-icon>
+          <span>{{ errorPasswordMsg }}</span>
+        </div>
+      </v-form>
+
+      <v-btn class="fn-right btn--margin-t30 btn--info btn--space" @click="passwordUpdate">
+        {{ $t('confirm', $store.state.locale) }}
+      </v-btn>
+    </div>
+    <br>
+    <div class="card fn-clear card__body">
+      <v-form ref="form">
+        <v-text-field
+          :label="$t('avatarURL', $store.state.locale)"
+          v-model="avatarURL"
+          :rules="requiredRules"
+          @keyup.ctrl.13="accountUpdate"
+          @keyup.meta.13="accountUpdate"
+        ></v-text-field>
+
+        <v-text-field
+          label="B3Key"
+          v-model="b3key"
+          :rules="requiredRules"
+          @keyup.ctrl.13="accountUpdate"
+          @keyup.meta.13="accountUpdate"
+        ></v-text-field>
+
+        <div class="alert alert--danger" v-show="error">
+          <v-icon>danger</v-icon>
+          <span>{{ errorMsg }}</span>
+        </div>
+      </v-form>
+
+      <v-btn class="fn-right btn--margin-t30 btn--info btn--space" @click="accountUpdate">
+        {{ $t('confirm', $store.state.locale) }}
+      </v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+  import sha512crypt from 'sha512crypt-node'
+  import { required } from '~/plugins/validate'
+
+  export default {
+    data () {
+      return {
+        requiredRules: [
+          (v) => required.call(this, v)
+        ],
+        error: false,
+        errorMsg: '',
+        errorPassword: false,
+        errorPasswordMsg: '',
+        password: '',
+        b3key: '',
+        avatarURL: ''
+      }
+    },
+    head () {
+      return {
+        title: `${this.$t('account', this.$store.state.locale)} - ${this.$store.state.blogTitle}`
+      }
+    },
+    methods: {
+      async passwordUpdate () {
+        if (!this.$refs.passwordForm.validate()) {
+          return
+        }
+        const responseData = await this.axios.put('/console/settings/account/password', {
+          password: sha512crypt.sha512crypt(this.password, `$6$5000$${Math.random().toString(36)}`)
+        })
+
+        if (responseData.code === 0) {
+          this.$set(this, 'errorPassword', false)
+          this.$set(this, 'errorPasswordMsg', '')
+          this.$store.commit('setSnackBar', {
+            snackBar: true,
+            snackMsg: this.$t('setupSuccess', this.$store.state.locale),
+            snackModify: 'success'
+          })
+        } else {
+          this.$set(this, 'errorPassword', true)
+          this.$set(this, 'errorPasswordMsg', responseData.msg)
+        }
+      },
+      async accountUpdate () {
+        if (!this.$refs.form.validate()) {
+          return
+        }
+        const responseData = await this.axios.put('/console/settings/account', {
+          b3key: this.b3key,
+          avatarURL: this.avatarURL
+        })
+
+        if (responseData.code === 0) {
+          this.$set(this, 'error', false)
+          this.$set(this, 'errorMsg', '')
+          this.$store.commit('setSnackBar', {
+            snackBar: true,
+            snackMsg: this.$t('setupSuccess', this.$store.state.locale),
+            snackModify: 'success'
+          })
+        } else {
+          this.$set(this, 'error', true)
+          this.$set(this, 'errorMsg', responseData.msg)
+        }
+      }
+    },
+    async mounted () {
+      const responseData = await this.axios.get('/console/settings/account')
+      if (responseData) {
+        this.$set(this, 'b3key', responseData.b3Key)
+        this.$set(this, 'avatarURL', responseData.avatarURL)
+      }
+    }
+  }
+</script>
