d57ebc9be82b5cbaa895a87ee49ce05d6c221369
Liang Ding <d@b3log.org>
2018-07-14 14:14:59 +0800

    :sparkles: #132

--- controller/articlectl.go
+++ controller/articlectl.go
@@ -70,6 +70,9 @@ func showArticlesAction(c *gin.Context) {
 		if strconv.Itoa(model.SettingPreferenceArticleListStyleValueTitleAbstract) == articleListStyleSetting.Value {
 			abstract = template.HTML(mdResult.AbstractText)
 		}
+		if "" != articleModel.Abstract {
+			abstract = template.HTML(articleModel.Abstract)
+		}
 		if strconv.Itoa(model.SettingPreferenceArticleListStyleValueTitleContent) == articleListStyleSetting.Value {
 			abstract = template.HTML(mdResult.ContentHTML)
 			thumbnailURL = ""
