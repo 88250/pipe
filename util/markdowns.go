fbf12eaa56fb1c379e93e1753998c3cd9205e966
Liang Ding <d@b3log.org>
2018-11-01 22:05:49 +0800

    :bug: Fix #156

--- util/markdowns.go
+++ util/markdowns.go
@@ -108,6 +108,8 @@ func bf(mdText string) []byte {
 
 // Markdown process the specified markdown text to HTML.
 func Markdown(mdText string) *MarkdownResult {
+	mdText = strings.Replace(mdText, "\r\n", "\n", -1)
+
 	digest := md5.New()
 	digest.Write([]byte(mdText))
 	key := string(digest.Sum(nil))
