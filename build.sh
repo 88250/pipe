0b4b7e74c1bd972297b7c662006ab99cd7253889
Van <lly219@gmail.com>
2018-11-08 11:17:35 +0800

    :art:  about #159

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 go build -i -v
 cd console && npm install && npm run build
-cd ../theme && npm install && npm install --global gulp && gulp
+cd ../theme && npm install && npm run build
 
 echo 'build pipe done'
