18f5c3b5dd5fd6fe141eba110c269ad5675da57b
Van <lly219@gmail.com>
2018-11-08 11:12:54 +0800

    :art:  fixed #159

--- theme/gulpfile.js
+++ theme/gulpfile.js
@@ -2,99 +2,112 @@
  * @file themes tool.
  *
  * @author <a href='http://vanessa.b3log.org'>Liyuan Li</a>
- * @version 0.1.0.1, Dec 27, 2017
+ * @version 1.0.0.0, Nov 8, 2018
  */
 
 const gulp = require('gulp')
 const sass = require('gulp-sass')
 const rename = require('gulp-rename')
-const composer = require('gulp-uglify/composer')
-const uglifyjs = require('uglify-es')
-const gulpUtil = require('gulp-util')
+var uglify = require('gulp-uglify')
 const fs = require('fs')
 const browserify = require('browserify')
-const livereload = require('gulp-livereload')
 const source = require('vinyl-source-stream')
 const buffer = require('vinyl-buffer')
-const sourcemaps = require('gulp-sourcemaps')
 const es = require('event-stream')
 
-gulp.task('sass', function () {
-  gulp.src('./scss/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('./scss'))
+function themeSassProcess () {
+  return gulp.src('./x/*/css/*.scss').
+    pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError)).
+    pipe(gulp.dest('./x'))
+}
 
-  return gulp.src('./x/*/css/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('./x'))
-})
+function themeSassProcessWatch () {
+  gulp.watch('./x/*/css/*.scss', themeSassProcess)
+}
 
-gulp.task('dev', function () {
-  const theme = gulpUtil.env.theme || '9IPHP'
-  const files = [`./x/${theme}/js/common.js`, `./x/${theme}/js/article.js`]
+function baseSassProcess () {
+  return gulp.src('./scss/*.scss').
+    pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError)).
+    pipe(gulp.dest('./scss'))
+}
 
-  const tasks = files.map(function (entry) {
-    return browserify({entries: [entry]})
-      .transform('babelify', {presets: ['es2015']})
-      .bundle()
-      .pipe(source(entry))
-      .pipe(rename({suffix: '.min'}))
-      .pipe(buffer())
-      .pipe(sourcemaps.init())
-      .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest('.'))
-      .pipe(livereload())
+function baseSassProcessWatch () {
+  gulp.watch('./scss/*.scss', baseSassProcess)
+}
+
+function devJSProcessWatch () {
+  const watcher = gulp.watch([
+    './x/*/js/article.js',
+    './x/*/js/common.js'])
+
+  watcher.on('change', function (entry) {
+    browserify({entries: [entry]}).
+      transform('babelify', {presets: ['@babel/preset-env']}).
+      bundle().
+      on('error', function (err) { console.error(err) }).
+      pipe(source(entry)).
+      pipe(rename({suffix: '.min'})).
+      pipe(buffer()).
+      pipe(gulp.dest('.'))
+    console.log('[Packed] ' + entry)
   })
-  return es.merge.apply(null, tasks)
-})
+}
 
-gulp.task('watch', function () {
-  livereload.listen()
-  gulp.watch(['./js/common.js', './js/article.js', './x/*/js/article.js', './x/*/js/common.js', './x/*/js/symbol.js'], ['dev'])
-  gulp.watch(['./x/*/css/*.scss', './scss/*.scss'], ['sass'])
-})
+gulp.task('watch',
+  gulp.parallel(themeSassProcessWatch, baseSassProcessWatch, devJSProcessWatch))
 
-gulp.task('build', function () {
+function updateVersion () {
   // set static version
   const newVersion = (new Date()).getTime()
 
   // set pipe.json
   fs.writeFileSync('../pipe.json',
-    fs.readFileSync('../pipe.json', 'UTF-8')
-      .replace(/"StaticResourceVersion": "\d{13}"/, `"StaticResourceVersion": "${newVersion}"`), 'UTF-8')
+    fs.readFileSync('../pipe.json', 'UTF-8').
+      replace(/"StaticResourceVersion": "\d{13}"/,
+        `"StaticResourceVersion": "${newVersion}"`), 'UTF-8')
 
-  const minify = composer(uglifyjs)
   // min sw.js
-  browserify({entries: `./sw.js`})
-    .transform('babelify', {presets: ['es2015']})
-    .bundle()
-    .pipe(source('sw.min.js'))
-    .pipe(buffer())
-    .pipe(minify().on('error', gulpUtil.log))
-    .pipe(gulp.dest('.'))
+  return browserify({entries: `./sw.js`}).
+    transform('babelify', {presets: ['@babel/preset-env']}).
+    bundle().
+    on('error', function (err) { console.error(err) }).
+    pipe(source('sw.min.js')).
+    pipe(buffer()).
+    pipe(uglify()).
+    pipe(gulp.dest('.'))
+}
 
-  // theme js
+function minThemeJS () {
   fs.readdirSync('./x').forEach(function (file) {
     const jsPath = `./x/${file}/js/`
     try {
       fs.statSync(`${jsPath}/common.js`)
-      browserify({entries: `${jsPath}/common.js`})
-        .transform('babelify', {presets: ['es2015']})
-        .bundle()
-        .pipe(source('common.min.js'))
-        .pipe(buffer())
-        .pipe(minify().on('error', gulpUtil.log))
-        .pipe(gulp.dest(jsPath))
-      browserify({entries: `${jsPath}/article.js`})
-        .transform('babelify', {presets: ['es2015']})
-        .bundle()
-        .pipe(source('article.min.js'))
-        .pipe(buffer())
-        .pipe(minify().on('error', gulpUtil.log))
-        .pipe(gulp.dest(jsPath))
+      browserify({entries: [`${jsPath}/common.js`]}).
+        transform('babelify', {presets: ['@babel/preset-env']}).
+        bundle().
+        on('error', function (err) { console.error(err) }).
+        pipe(source(`${jsPath}/common.js`)).
+        pipe(rename({suffix: '.min'})).
+        pipe(buffer()).
+        pipe(uglify()).
+        pipe(gulp.dest('.'))
+      fs.statSync(`${jsPath}/article.js`)
+      browserify({entries: [`${jsPath}/article.js`]}).
+        transform('babelify', {presets: ['@babel/preset-env']}).
+        bundle().
+        on('error', function (err) { console.error(err) }).
+        pipe(source(`${jsPath}/article.js`)).
+        pipe(rename({suffix: '.min'})).
+        pipe(buffer()).
+        pipe(uglify()).
+        pipe(gulp.dest('.'))
     } catch (e) {
     }
   })
-})
+  // fixed gulp task return error
+  return gulp.src('./sw.js');
+}
 
-gulp.task('default', ['sass', 'build'])
+gulp.task('default',
+  gulp.series(updateVersion,
+    gulp.parallel(themeSassProcess, baseSassProcess, minThemeJS)))
