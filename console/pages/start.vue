f315cce6bb418e776342853b9f805953489cdf5b
Van <lly219@gmail.com>
2019-02-14 17:08:00 +0800

    :memo: add intro

--- console/pages/start.vue
+++ console/pages/start.vue
@@ -1,14 +1,28 @@
 <template>
   <div class="console" id="particles">
-    <div class="card" ref="content">
-      <div class="login__content">
-        <div class="fn__pointer" @click="loginGitHub">
-          <br/><br/>
-          <v-btn class="btn--small btn--info">{{ $t('index2', $store.state.locale) }}</v-btn>
-          <div class="login__github"></div>
-          <img class="fn__none" src="~assets/images/github.gif"/>
-        </div>
+    <div class="card login__content" ref="content">
+      <div class="login__github" @click="loginGitHub"></div>
+      <img class="fn__none" src="~assets/images/github.gif"/>
+      <label class="checkbox ft__12">
+        <input
+          type="checkbox"
+          :checked="isAgreen"
+          @click="isAgreen = !isAgreen"/><span
+        class="checkbox__icon"></span>
+        是否愿意在 GitHub 上收藏该项目、关注<a href="https://github.com/88250" target="_blank">开发者</a>并加入
+        <a href="https://github.com/b3log" target="_blank">B3log</a> 开源组织
+      </label> <br>
+      <v-btn class="btn--small btn--info" @click="loginGitHub">{{ $t('index2', $store.state.locale) }}</v-btn>
+      <div class="start__space"></div>
+      <a class="ft__12 fn__pointer" @click="toggleIntro">查看 GitHub 数据使用说明</a>
+      <div class="pipe-content__reset ft__12 start__intro" v-show="showIntro">
+        <ul>
+          <li>获取用户名、昵称、头像、邮箱用于账号初始化</li>
+          <li>获取公开仓库信息用于展示</li>
+          <li>我们不会对你的数据进行任何写入操作</li>
+        </ul>
       </div>
+      <div class="start__space"></div>
     </div>
   </div>
 </template>
@@ -21,6 +35,8 @@
     data () {
       return {
         clickedGitHub: false,
+        isAgreen: true,
+        showIntro: false,
       }
     },
     head () {
@@ -29,6 +45,9 @@
       }
     },
     methods: {
+      toggleIntro () {
+        this.$set(this, 'showIntro', !this.showIntro)
+      },
       loginGitHub () {
         this.$store.commit('setSnackBar', {
           snackBar: true,
@@ -36,7 +55,9 @@
           snackModify: 'success',
         })
         if (!this.clickedGitHub) {
-          window.location.href = `${process.env.AxiosBaseURL}/oauth/github/redirect?referer=${document.referrer}`
+          window.location.href = `${process.env.AxiosBaseURL}/oauth/github/redirect?referer=${document.referrer}__${this.isAgreen
+            ? '0'
+            : '1'}`
           this.$set(this, 'clickedGitHub', true)
         }
       },
@@ -46,3 +67,15 @@
     },
   }
 </script>
+
+<style lang="sass">
+  .ft__12
+    font-size: 12px !important
+  .start
+    &__intro
+      text-align: left
+      width: 300px
+      margin: 0 auto
+    &__space
+      height: 20px
+</style>
