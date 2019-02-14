d9f5b4bc56af6fd0864654f57205061d20f957a7
Liang Ding <d@b3log.org>
2019-02-14 17:13:42 +0800

    :art: 登录问题

--- controller/oauthctl.go
+++ controller/oauthctl.go
@@ -60,7 +60,7 @@ func githubCallbackAction(c *gin.Context) {
 
 	referer := state[16:]
 	if strings.Contains(referer, "__0") || strings.Contains(referer, "__1") {
-		referer = referer[:len("__0")]
+		referer = referer[:len(referer)-len("__0")]
 	}
 	accessToken := c.Query("ak")
 	githubUser := util.GitHubUserInfo(accessToken)
