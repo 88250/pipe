f49fe7099d0694c5fc2484b1786ff7a12a0ae927
Liang Ding <d@b3log.org>
2019-03-28 16:59:47 +0800

    :art: #223

--- console/tool.js
+++ console/tool.js
@@ -8,7 +8,7 @@ const readDirSync = (root) => {
     if (info.isDirectory()) {
       readDirSync(root + '/' + file)
     } else {
-      if (path.extname(file) === '.js' || path.extname(file) === '.html') {
+      if (path.extname(file) === '.js' || path.extname(file) === '.html' || path.extname(file) === '.css') {
         fs.writeFileSync(`${root}/${file}.tpl`,
           fs.readFileSync(`${root}/${file}`))
         console.log(`gen tpl: ${root}/${file}.tpl`)
