d5f88f343bda51384723a4c1cf91362c108d6e55
Liang Ding <d@b3log.org>
2019-05-24 13:57:19 +0800

    :page_facing_up: 更新文件头

--- controller/articlectl.go
+++ controller/articlectl.go
@@ -1,5 +1,5 @@
 // Pipe - A small and beautiful blogging platform written in golang.
-// Copyright (C) 2017-2019, b3log.org & hacpai.com
+// Copyright (C) 2017-present, b3log.org
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
