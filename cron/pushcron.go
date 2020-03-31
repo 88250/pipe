bc74737fd9ec24d7062dc490b2a64f1e735fe34d
Liang Ding <d@b3log.org>
2020-03-31 20:26:50 +0800

    :art: 废弃评论推送社区接口 #27

--- cron/pushcron.go
+++ cron/pushcron.go
@@ -11,7 +11,6 @@
 package cron
 
 import (
-	"crypto/tls"
 	"net/url"
 	"time"
 
@@ -19,7 +18,6 @@ import (
 	"github.com/88250/pipe/model"
 	"github.com/88250/pipe/service"
 	"github.com/88250/pipe/util"
-	"github.com/parnurzeal/gorequest"
 )
 
 func pushArticlesPeriodically() {
@@ -43,67 +41,5 @@ func pushArticles() {
 	articles := service.Article.GetUnpushedArticles()
 	for _, article := range articles {
 		service.Article.ConsolePushArticle(article)
-	}
-}
-
-func pushCommentsPeriodically() {
-	go pushComments()
-
-	go func() {
-		for range time.Tick(time.Second * 30) {
-			pushComments()
-		}
-	}()
-}
-
-func pushComments() {
-	defer gulu.Panic.Recover(nil)
-
-	server, _ := url.Parse(model.Conf.Server)
-	if !util.IsDomain(server.Hostname()) {
-		return
-	}
-
-	comments := service.Comment.GetUnpushedComments()
-	for _, comment := range comments {
-		article := service.Article.ConsoleGetArticle(comment.ArticleID)
-		articleAuthor := service.User.GetUser(article.AuthorID)
-		b3Key := articleAuthor.B3Key
-		b3Name := articleAuthor.Name
-		if "" == b3Key {
-			continue
-		}
-
-		author := service.User.GetUser(comment.AuthorID)
-		blogTitleSetting := service.Setting.GetSetting(model.SettingCategoryBasic, model.SettingNameBasicBlogTitle, comment.BlogID)
-		requestJSON := map[string]interface{}{
-			"comment": map[string]interface{}{
-				"id":         comment.ID,
-				"articleId":  comment.ArticleID,
-				"content":    comment.Content,
-				"authorName": author.Name,
-			},
-			"client": map[string]interface{}{
-				"name":      "Pipe",
-				"ver":       model.Version,
-				"title":     blogTitleSetting.Value,
-				"host":      model.Conf.Server,
-				"userName":  b3Name,
-				"userB3Key": b3Key,
-			},
-		}
-		result := &map[string]interface{}{}
-		_, _, errs := gorequest.New().TLSClientConfig(&tls.Config{InsecureSkipVerify: true}).
-			Post("https://rhythm.b3log.org/api/comment").SendMap(requestJSON).
-			Set("user-agent", model.UserAgent).Timeout(30*time.Second).
-			Retry(3, 5*time.Second).EndStruct(result)
-		if nil != errs {
-			logger.Errorf("push a comment to Rhy failed: " + errs[0].Error())
-		} else {
-			logger.Infof("push a comment to Rhy result: %+v", result)
-		}
-		comment.PushedAt = comment.UpdatedAt
-
-		service.Comment.UpdatePushedAt(comment)
 	}
 }
